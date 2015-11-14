6f794adaadb26e35e966027b0b2df678f5fa355b
Ronin_X <voroenckif@mail.ru>
2015-11-14 21:05:20 +0300

    edit

--- _array/arrays.cs
+++ _array/arrays.cs
@@ -217,7 +217,7 @@ namespace _array
             }
             return (mass1);
         }
-        //123121212312311111
+        //1231212123123123132
         public double apr_2x2(arrays mass)
         {
             return (mass.data[0, 0] * mass.data[1, 1] - mass.data[0, 1] * mass.data[1, 0]);
